e4b4a30923d9aa86c00813392a8ac58341fbb133
kyruski <jamescboyett@gmail.com>
2019-02-06 14:17:36 -0600

    Adds directory filesystem and index.js for the server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.listen(3000);
