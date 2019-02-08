821aacb3b7578c2ba40ed67f3f33211803ba1a22
James Boyett <44087679+Kyruski@users.noreply.github.com>
2019-02-08 13:14:28 -0600

    Revert "Adds basic functionality to view reviews"

--- db/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const environment = process.env.ENVIRONMENT || 'development'
-const config = require('../knexfile.js');
-const knex = require('knex')(config[environment]);
-
-module.exports = knex;
-
-knex.migrate.latest([config]);
