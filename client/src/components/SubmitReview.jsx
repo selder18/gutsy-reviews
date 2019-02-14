0faf2cb359ebf41e7b4e3391da893d4a4874c812
kyruski <jamescboyett@gmail.com>
2019-02-14 12:37:17 -0600

    Refactors to use hooks over classes (React)

--- client/src/components/SubmitReview.jsx
+++ client/src/components/SubmitReview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 import StarsRating from 'react-star-ratings'
 import faker from 'faker';
@@ -6,105 +6,93 @@ import moment from 'moment';
 import { submit } from '../../style.js';
 import '@babel/polyfill';
 
-class SubmitReview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: 0
-    }
-  }
+const SubmitReview = (props) => {
+  const [rating, setRating] = useState(0),
 
-  async insertUser(body) {
-    let result = await Axios.post('/query', body);
-    return result.data;
-  }
+    insertUser = async (body) => {
+      let result = await Axios.post('/query', body);
+      return result.data;
+    },
 
-  makeUsersBody(name) {
-    return {
-      table: 'users',
-      payload: {
-        name,
-        avatar: faker.image.avatar()
+    makeUsersBody = (name) => (
+      {
+        table: 'users',
+        payload: {
+          name,
+          avatar: faker.image.avatar()
+        }
       }
-    };
-  }
+    ),
 
-  async getPosterID(name) {
-    let result = await Axios.get(`/query/user/${name}`);
-    if (result.data.length) { //if the user exists, return the id
-      return result.data[0].id;
-    } else { //else if user doesn't exist, make the user
-      let body = this.makeUsersBody(name); //make the body to submit in the POST request
-      let test = await this.insertUser(body); //and then return the id of the inserted user
-      return test[0].id;
-    }
-  }
-
-  makeReviewBody(name, review, date, stars) {
-    return {
-      table: 'reviews',
-      payload: {
-        adventure_id: this.props.adventure_id,
-        poster_id: name,
-        review_text: review,
-        timestamp: date,
-        stars: stars
+    getPosterID = async (name) => {
+      let result = await Axios.get(`/query/user/${name}`);
+      if (result.data.length) { //if the user exists, return the id
+        return result.data[0].id;
+      } else { //else if user doesn't exist, make the user
+        let body = makeUsersBody(name); //make the body to submit in the POST request
+        let test = await insertUser(body); //and then return the id of the inserted user
+        return test[0].id;
       }
+    },
+
+    makeReviewBody = (name, review, date, stars) => {
+      return {
+        table: 'reviews',
+        payload: {
+          adventure_id: props.adventure_id,
+          poster_id: name,
+          review_text: review,
+          timestamp: date,
+          stars: stars
+        }
+      };
+    },
+
+    resetReviewFields = () => {
+      document.getElementById('submitText').value = '';
+      document.getElementById('submitUsername').value = '';
+      props.getReviews()
+      setRating(0);
+    },
+
+
+
+    postReview = async () => {
+      const review = document.getElementById('submitText').value, //grab review text
+        username = document.getElementById('submitUsername').value, //grab username
+        name = await getPosterID(username), //get id of user/make new user (see above function)
+        stars = rating, //grab stars
+        date = moment().format(), //find current date
+        body = makeReviewBody(name, review, date, stars); //makes the body for submitting reviews
+      Axios.post('/query', body)
+        .then(() => {
+          resetReviewFields();
+        })
+        .catch((err) => { throw err });
+    },
+
+    submitReview = (e) => {
+      e.preventDefault();
+      postReview();
     };
-  }
 
-  resetReviewFields() {
-    document.getElementById('submitText').value = '';
-    document.getElementById('submitUsername').value = '';
-    this.props.getReviews()
-    this.setState({
-      rating: 0
-    });
-  }
+  return (
+    <div style={submit.main}>
+      <form id="submitReview">
+        <input id="submitUsername" type="text" name="username" placeholder="Username" style={submit.username} />
+        &nbsp;&nbsp;
+        <StarsRating name="reviewStars" rating={rating} starRatedColor={"#ffa534"} starHoverColor={"#ffa534"} starEmptyColor={'grey'} changeRating={setRating.bind(this)} starDimension={'15px'} starSpacing={'0px'} />
+        <br />
+        <textarea id="submitText" placeholder="Write your review of this Gutsy adventure here!" style={submit.text} />
+        &nbsp;
+        <input type="Submit" defaultValue="Submit" style={submit.button} onClick={(e) => {
+          submitReview(e);
+        }} />
+        <br />
+      </form>
+    </div>
+  );
 
-  async postReview() {
-    const review = document.getElementById('submitText').value, //grab review text
-      username = document.getElementById('submitUsername').value, //grab username
-      name = await this.getPosterID(username), //get id of user/make new user (see above function)
-      stars = this.state.rating, //grab stars
-      date = moment().format(), //find current date
-      body = this.makeReviewBody(name, review, date, stars); //makes the body for submitting reviews
-    Axios.post('/query', body)
-      .then(() => {
-        this.resetReviewFields();
-      })
-      .catch((err) => { throw err });
-  }
-
-  changeRating(newRating) {
-    this.setState({
-      rating: newRating
-    });
-  }
-
-  submitReview(e) {
-    e.preventDefault();
-    this.postReview();
-  }
-
-  render() {
-    return (
-      <div style={submit.main}>
-        <form id="submitReview">
-          <input id="submitUsername" type="text" name="username" placeholder="Username" style={submit.username} />
-          &nbsp;&nbsp;
-          <StarsRating name="reviewStars" rating={this.state.rating} starRatedColor={"#ffa534"} starHoverColor={"#ffa534"} starEmptyColor={'grey'} changeRating={this.changeRating.bind(this)} starDimension={'15px'} starSpacing={'0px'} />
-          <br />
-          <textarea id="submitText" placeholder="Write your review of this Gutsy adventure here!" style={submit.text} />
-          &nbsp;
-          <input type="Submit" defaultValue="Submit" style={submit.button} onClick={(e) => {
-            this.submitReview(e);
-          }} />
-          <br />
-        </form>
-      </div>
-    );
-  }
 }
 
 export default SubmitReview;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,47 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
 import SubmitReview from './components/SubmitReview.jsx'
 import ReviewsList from './components/ReviewsList.jsx';
 import { indexStyling } from '../style.js';
 
-class Reviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: this.props.id || 1,
-      reviews: []
-    }
-  }
+const Reviews = (props) => {
+  const [id, setId] = useState(props.id || 1),
+    [reviews, setReviews] = useState([]);
 
-  getReviews() {
-    Axios.get(`/query/reviews/${this.state.id}`)
+  const getReviews = () => {
+    Axios.get(`/query/reviews/${id}`)
       .then((data) => {
-        this.setState({
-          reviews: data.data
-        })
+        setReviews(data.data);
       })
-  }
+      .catch((err) => { throw err });
+  };
 
-  componentDidMount() {
-    this.getReviews();
-  }
+  useEffect(() => {
+    getReviews();
+  }, id); //only want to update if the id changes, else we invoke getReviews manually
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState === this.state) {
-      this.getReviews();
-    }
-  }
-
-  render() {
-    return (
-      <div style={indexStyling}>
-        <SubmitReview adventure_id={this.state.id} getReviews={this.getReviews.bind(this)} />
-        <ReviewsList reviews={this.state.reviews} />
-      </div>
-    );
-  }
-}
+  return (
+    <div style={indexStyling}>
+      <SubmitReview adventure_id={id} getReviews={getReviews.bind(this)} />
+      <ReviewsList reviews={reviews} />
+    </div>
+  );
+};
 
 ReactDOM.render(<Reviews />, document.getElementById('reviews'));
 
