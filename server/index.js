84c098a59485e10056a04498a8a5f576d124bd37
kyruski <jamescboyett@gmail.com>
2019-02-18 20:01:41 -0600

    Fixes a warning with the keys

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get('/query/reviews/:id', (req, res) => {
   db('reviews')
     .join('users', 'reviews.poster_id', '=', 'users.id') // join table for users
     .join('adventures', 'reviews.adventure_id', '=', 'adventures.id') // join table for adventures
-    .select({username: 'users.name', adventure: 'adventures.name'}, 'users.avatar', 'reviews.review_text', 'reviews.timestamp', 'reviews.stars', 'reviews.thumbs_up', 'reviews.thumbs_down') // changes users.name to username and adventures.name to adventure, selects all required fields
+    .select({username: 'users.name', adventure: 'adventures.name'}, 'reviews.id', 'users.avatar', 'reviews.review_text', 'reviews.timestamp', 'reviews.stars', 'reviews.thumbs_up', 'reviews.thumbs_down') // changes users.name to username and adventures.name to adventure, selects all required fields
     .where('reviews.adventure_id', req.params.id)
     .then((data) => {
       res.json(data);
