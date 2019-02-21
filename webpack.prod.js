9a32ebdf2a99fbc12c524657515f7280bd2c606e
kyruski <jamescboyett@gmail.com>
2019-02-20 18:38:18 -0600

    Alters the webpack and pm2 scrits

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,26 +3,11 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); //eslint-di
 const MinifyPlugin = require('babel-minify-webpack-plugin'); //eslint-disable-line
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); //eslint-disable-line
 const CompressionPlugin = require('compression-webpack-plugin'); //eslint-disable-line
-// const path = require('path');
 const common = require('./webpack.common');
-
-// const SRC_DIR = path.join(__dirname, '/client/src');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [new BundleAnalyzerPlugin(),
-    // new MinifyPlugin({}, {
-    //   test: /\.jsx?/,
-    //   include: SRC_DIR
-    // }),
-    new CompressionPlugin({
-      // test: /\.jsx?/,
-      // algorithm: 'gzip',
-      // include: SRC_DIR,
-      // threshold: 10240,
-      // minRatio: 0.8
-    })
-  ],
+  plugins: [new CompressionPlugin()],
   devtool: '',
 
 });
