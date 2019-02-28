d47cc0029dc512d3a871a7b3e193a3fbe584248c
Scott Elder <smelder@gmail.com>
2019-02-28 14:21:48 -0600

    Postgres schemas complete

--- knex/migrations/20190227165613_comments.js
+++ knex/migrations/20190227165613_comments.js
@@ -1,17 +1,22 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
-      table.string('username').notNullable();
-      table.string('avatar').notNullable(); //This is a string because it will refer to a location to look up an image
+    knex.schema.createTable('users', (users) => {
+      users.increments('id').primary();
+      users.string('username').notNullable();
+      users.string('avatar').notNullable(); //This is a string because it will refer to a location to look up an image
     }),
-    knex.schema.createTable('adventures', (table) => {
-      table.increments('id').primary();
-      table.string('title').notNullable();
+    knex.schema.createTable('adventures', (adventures) => {
+      adventures.increments('id').primary();
+      adventures.string('title').notNullable();
     }),
-    knex.schema.createTable('review', (table) => {
-      
+    knex.schema.createTable('reviews', (reviews) => {
+      reviews.increments('id').primary();
+      reviews.timestamp('created_at').defaultTo(knex.fn.now());
+      reviews.integer('stars');
+      reviews.string('comments').notNullable();
+      reviews.foreign('user_id').references('id').inTable('users').notNullable();
+      reviews.foreign('adventure_id').reference('id').inTable('adventures').notNullable();
     })
   ])
 };
