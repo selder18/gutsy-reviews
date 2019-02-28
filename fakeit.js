625895ccff275680083ea00b368c283afeaebffe
Scott Elder <smelder@gmail.com>
2019-02-28 17:22:52 -0600

    Faker set up as object with faking methods

--- fakeit.js
+++ fakeit.js
@@ -1,16 +1,37 @@
 const faker = require('faker');
 
 const fakeIt = {
-  makeFakeUsers = () => {
-    faker.fake()
+  makeUsers: () => {
+    const users = [];
+    const userObj = {};
+    for (let i = 0; i < 10000; i++) {
+      userObj.username = faker.internet.userName();
+      userObj.avatar = faker.image.avatar();
+      users.push(userObj);
+    }
+    return users;
   },
 
-  makeFakeAdventures = () => {
-    faker.fake()
+  makeAdventures: () => {
+    const adventures = [];
+    const adventureObj = {};
+    for (let i = 0; i < 10000; i++) {
+      adventureObj.title = faker.company.catchPhrase();
+      adventures.push(adventureObj);
+    }
+    return adventures;
   },
 
-  makeFakeReviews = () => {
-    faker.fake()
+  makeReviews: () => {
+    const reviews = [];
+    const reviewObj = {};
+    for (let i = 0; i < 10000; i++) {
+      reviewObj.timeStamp = faker.date.past(3000);
+      reviewObj.comment = faker.lorem.sentences(3);
+      reviewObj.stars = faker.random.number({min:0, max:5});
+      reviews.push(reviewObj);
+    }
+    return reviews;
   }
 }
 
