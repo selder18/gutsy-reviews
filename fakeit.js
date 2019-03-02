a95387844a88826dd47188bf27b1bcf5c291c063
Scott Elder <smelder@gmail.com>
2019-03-02 13:50:57 -0600

    Expanded fakeit.js to cover adventures and reviews

--- fakeit.js
+++ fakeit.js
@@ -36,6 +36,17 @@ const fakeIt = {
       }
     return adventures;
   },
+  
+  makeAdventuresArray: () => {
+    const adventures = [];
+    for (let i = 0; i < 1000; i++) {
+      const adventuresArray = [
+        faker.company.catchPhrase()
+      ];
+      adventures.push(adventuresArray);
+    }
+    return adventures;
+  },
 
   makeReviews: () => {
     const reviews = [];
@@ -47,6 +58,20 @@ const fakeIt = {
         user_id: Math.ceil(Math.random() * 100), //Must be .ceil to avoid problems trying to assign to 0
         adventure_id: Math.ceil(Math.random() * 100) //Must be .ceil to avoid problems trying to assign to 0
     }
+    reviews.push(reviewObj);
+    }
+    return reviews;
+  },
+  makeReviewsArray: () => {
+    const reviews = [];
+    for (let i = 0; i < 1000; i++) {
+      const reviewObj = [
+        faker.date.past(3000),
+        faker.random.number({min:0, max:5}),
+        faker.lorem.sentences(Math.ceil(Math.random() * 3)),
+        Math.ceil(Math.random() * 1000000), //Must be .ceil to avoid problems trying to assign to 0
+        Math.ceil(Math.random() * 1000000) //Must be .ceil to avoid problems trying to assign to 0
+      ]
     reviews.push(reviewObj);
     }
     return reviews;
@@ -63,24 +88,33 @@ const csvPreparation = (array) => {
 };
 
 const csvContentsUsers = csvPreparation(fakeIt.makeUsersArray());
-// const csvContentsAdventures = csvPreparation(fakeIt.makeAdventures());
-// const csvContentsReviews = csvPreparation(fakeIt.makeReviews());
+const csvContentsAdventures = csvPreparation(fakeIt.makeAdventuresArray());
+const csvContentsReviews = csvPreparation(fakeIt.makeReviewsArray());
 
-const writeUsers = (string) => {
-  fs.writeFile('./users.csv', string, (err, data) => {
-    if (err) console.log(err)
-    else console.log('Successfully wrote to file');
-  })
-};
-
-const appendUsers = (bigString) => {
-  const stream = fs.createWriteStream('./users.csv', {flags: 'a'})
-  for (let i = 0; i < 9999; i++) {
+const writeUsers = (bigString) => {
+  const stream = fs.createWriteStream('./users.csv');
+  for (let i = 0; i < 1000; i++) {
     stream.write(bigString.substring(17)); //this is to cut out the first 'formatting' line of the CSV
   }
 }
 
+const writeAdventures = (bigString) => {
+  const stream = fs.createWriteStream('./adventures.csv');
+  for (let i = 0; i < 1000; i++) {
+    stream.write(bigString.substring(17)) //this is to cut out the first 'formatting' line of the CSV
+  }
+}
+
+const writeReviews = (bigString) => {
+  const stream = fs.createWriteStream('./reviews.csv');
+  for (let i = 0; i < 10000; i++) {
+    stream.write(bigString.substring(17))
+  }
+}
+
 writeUsers(csvContentsUsers);
-appendUsers(csvContentsUsers);
+writeAdventures(csvContentsAdventures);
+writeReviews(csvContentsReviews);
+
 
 module.exports = fakeIt;
