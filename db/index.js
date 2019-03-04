ba5e5bb2f751c1f4bb82baa8abfff0b43ca6aacd
Scott Elder <smelder@gmail.com>
2019-03-04 14:51:07 -0600

    Reorganized file structure

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const knexConfig = require('../knexfile.js');
+const knexConfig = require('../knexfile.js')
 // const environment = process.env.NODE_ENV || 'production';
 // const config = require('../knexfile.js');
 const knex = require('knex');
